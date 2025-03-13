d0ef59e059430cedc22a4988131cf8a72a0c62b0
wanderleymp <wanderley@agilegestaoempresarial.com.br>
2025-03-13 11:12:58 -0400

    teste

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -89,7 +89,7 @@ export function Login() {
             <div className="space-y-4">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
-                  Email
+                  E-mail
                 </label>
                 <div className="relative">
                   <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
